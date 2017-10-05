62f86694f0d9f577dbd84e641560134c8ecdf5e4
Oksana Shadura <oksana.shadura@cern.ch>
2017-10-05 18:09:55 +0200

    Integration of rootbench repository into ROOT

---
+++ cmake/modules/RootBenchCTest.cmake
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+#
+#  RootBenchCTest.cmake
+#
+#  Setup file for CTest.
+#
+#-------------------------------------------------------------------------------
+
+# Set the CTest build name.
+set(CTEST_BUILD_NAME ${ROOT_ARCHITECTURE}-${CMAKE_BUILD_TYPE})
+#message("-- Set CTest build name to: ${CTEST_BUILD_NAME}")
+
+# Enable the creation and submission of dashboards.
+include(CTest)
+
+# Enable testing for CTest.
+enable_testing()
+
+# Copy the CTestCustom.cmake file into the build directory.
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/CTestCustom.cmake ${CMAKE_CURRENT_BINARY_DIR} COPYONLY)
