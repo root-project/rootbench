1290e6f308ebc532eac47b123b1985e1bbaef1f6
Oksana Shadura <oksana.shadura@cern.ch>
2017-11-23 12:18:50 +0100

    Extending set of benchmarks for interpreter

--- root/interpreter/RunInterpreter.cxx
+++ root/interpreter/RunInterpreter.cxx
@@ -24,9 +24,15 @@ static void TestTutorial(benchmark::State &state, const char *dir, const char *t
 
 BENCHMARK_CAPTURE(TestTutorial, Test_hsimple, "tutorials/", "hsimple.C")->Unit(benchmark::kMicrosecond);
 BENCHMARK_CAPTURE(TestTutorial, Test_TMVAGAexample, "tutorials/", "tmva/TMVAGAexample.C")->Unit(benchmark::kMicrosecond);
+BENCHMARK_CAPTURE(TestTutorial, Test_TMVAGAexample2, "tutorials/", "tmva/TMVAGAexample2.C")->Unit(benchmark::kMicrosecond);
 BENCHMARK_CAPTURE(TestTutorial, Test_hvector, "tutorials/", "tree/hvector.C")->Unit(benchmark::kMicrosecond);
 BENCHMARK_CAPTURE(TestTutorial, Test_hinvertMatrix, "tutorials/", "matrix/invertMatrix.C")->Unit(benchmark::kMicrosecond);
 BENCHMARK_CAPTURE(TestTutorial, Test_invertMatrix, "tutorials/", "math/invertMatrix.C")->Unit(benchmark::kMicrosecond);
 BENCHMARK_CAPTURE(TestTutorial, Test_fitLinear, "tutorials/", "fit/fitLinear.C")->Unit(benchmark::kMicrosecond);
+BENCHMARK_CAPTURE(TestTutorial, Test_roofit103, "tutorials/", "roofit/rf103_interprfuncs.C")->Unit(benchmark::kMicrosecond);
+BENCHMARK_CAPTURE(TestTutorial, Test_roofit105, "tutorials/", "roofit/rf105_funcbinding.C")->Unit(benchmark::kMicrosecond);
+BENCHMARK_CAPTURE(TestTutorial, Test_roofit109, "tutorials/", "roofit/rf109_chi2residpull.C")->Unit(benchmark::kMicrosecond);
+BENCHMARK_CAPTURE(TestTutorial, Test_roofit110, "tutorials/", "roofit/rf110_normintegration.C")->Unit(benchmark::kMicrosecond);
+BENCHMARK_CAPTURE(TestTutorial, Test_roofit204, "tutorials/", "roofit/rf204_extrangefit.C")->Unit(benchmark::kMicrosecond);
 
 BENCHMARK_MAIN();
