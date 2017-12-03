fb8ba880c560b06efb0cec4daff80e8d8e87f057
Oksana Shadura <oksana.shadura@cern.ch>
2017-12-03 17:33:37 +0100

    Adding memory measurements to interpreter benchmarks + tiny fix of typos

--- root/interpreter/InterpreterTest.h
+++ root/interpreter/InterpreterTest.h
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
 
 #include <unistd.h>
 
 #include "benchmark/benchmark.h"
 
-static int runTutorial(const std::string& dir, const std::string& filename) {
+static int runTutorial(const std::string& dir, const std::string& filename, const std::string& perffile) {
   if (getenv("ROOTSYS") == nullptr) {
     std::cerr << "Couldn't find ROOTSYS environment variable!\n";
     exit(1);
@@ -15,14 +18,19 @@ static int runTutorial(const std::string& dir, const std::string& filename) {
   std::string fullpath = rootsys + "/" + dir + "/" + filename;
   std::string thisroot = rootsys + "/bin/thisroot.sh";
   // FIXME: no source in /usr/dash
-  std::string rootInvocation = "source \"" + thisroot + "\" && root.exe -l -q -b -n -x \"" + fullpath + "\" -e return ";
+  // We are writing /usr/bin/time -v output in file to get maximum resident memory for the benchmark
+  std::string rootInvocation = "source \"" + thisroot + "\" && /usr/bin/time -v -o \"" + perffile + "\" root.exe -l -q -b -n -x \"" + fullpath + "\" -e return ";
     return std::system(rootInvocation.c_str());
 }
 
 static void TestTutorial(benchmark::State &state, const char *dir, const char *tutorial) {
-  // size_t peakSize = 0;
+  int peakSize = 0;
+  std::string perftutorial ("perfile.txt");
   for(auto _ : state){
-      runTutorial(dir, tutorial);
+      runTutorial(dir, tutorial,perftutorial);
+      std::string memorytutorial = "cat \"" + perftutorial + "\"| grep 'Maximum resident set size' | awk '{print $6}' > tmp_mem_file";
+      std::system(memorytutorial.c_str());
+      std::ifstream("tmp_mem_file") >> peakSize;
    }
-  // state.counters["PeakRSS"] = peakSize;
+   state.counters.insert({{"RSS", peakSize}});
 }
