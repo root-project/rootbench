7e8f93fd1aab330df4c459e4b9ecd1fb19064afd
Oksana Shadura <oksana.shadura@cern.ch>
2017-12-23 10:53:38 +0100

    Testing manual timing for Interpreter benchmarks

--- root/interpreter/InterpreterTest.h
+++ root/interpreter/InterpreterTest.h
@@ -9,6 +9,8 @@
 
 #include "benchmark/benchmark.h"
 
+#include <chrono>
+
 static int runTutorial(const std::string& dir, const std::string& filename, const std::string& perffile) {
   if (getenv("ROOTSYS") == nullptr) {
     std::cerr << "Couldn't find ROOTSYS environment variable!\n";
@@ -27,7 +29,13 @@ static void TestTutorial(benchmark::State &state, const char *dir, const char *t
   int peakSize = 0;
   std::string perftutorial ("perfile.txt");
   for(auto _ : state){
+      auto start = std::chrono::high_resolution_clock::now();
       runTutorial(dir, tutorial,perftutorial);
+      auto end   = std::chrono::high_resolution_clock::now();
+      auto elapsed_seconds =
+      std::chrono::duration_cast<std::chrono::duration<double>>(
+        end - start);
+      state.SetIterationTime(elapsed_seconds.count());
       std::string memorytutorial = "cat \"" + perftutorial + "\"| grep 'Maximum resident set size' | awk '{print $6}' > tmp_mem_file";
       std::system(memorytutorial.c_str());
       std::ifstream("tmp_mem_file") >> peakSize;
