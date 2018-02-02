5d8b743a02dfa765e0089842b7943326c7fcf7d8
Oksana Shadura <oksana.shadura@cern.ch>
2018-02-02 11:33:52 +0100

    Updating CMake options for rootbench.git

--- cmake/modules/RootBenchOptions.cmake
+++ cmake/modules/RootBenchOptions.cmake
@@ -1,1 +1,6 @@
-set(coverage "RootBenchmark coverage" OFF)
+#----------------------------------------------------------------------------
+# # Options for rootbench repository
+#
+# # TBD: to introduce special function for options (similar to root.git)
+#----------------------------------------------------------------------------
+set(coverage OFF)
