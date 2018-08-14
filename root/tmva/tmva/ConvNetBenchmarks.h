ff6529858c98ee64d22834e1683b22dae2617e8e
Emmanouil Stergiadis <em.stergiadis@gmail.com>
2018-08-14 10:58:49 +0200

    Now creating a dummy dataset on the fly rather than reading from disk

--- root/tmva/tmva/ConvNetBenchmarks.h
+++ root/tmva/tmva/ConvNetBenchmarks.h
@@ -4,46 +4,49 @@
 #include "TMVA/DataSetInfo.h"
 #include "TFile.h"
 #include "TTree.h"
-
+#include "MakeImageData.h"
 
 void CNN_ECAL_test(TString archName) {
 
-    int ntrainEvts = 5000;
-    int ntestEvts =  5000;
+   int ntrainEvts = 5000;
+   int ntestEvts =  5000;
 
-    ROOT::EnableImplicitMT(0);
-    TMVA::Config::Instance();
+   size_t nx = 32;
+   size_t ny = 32;
 
-    TFile *input(0);
+   ROOT::EnableImplicitMT(0);
+   TMVA::Config::Instance();
 
-    TString fname = "/media/manos/D/Work/rootbench/root/tmva/tmva/EcalData_n50000.root";
+   // Load the input data
+   TString fname = "imagesData.root";
+   TString fopt = "CACHEREAD";
 
-    input = TFile::Open( fname );
+   // Make some Gaussian Images.
+   makeImages(ntrainEvts + ntestEvts, nx, ny);
 
-    if (!input) {
-        std::cout << "ERROR: could not open data file" << std::endl;
-        exit(1);
-    }
+   auto input = TFile::Open(fname, fopt);
+    
+   R__ASSERT(input);
 
-    std::cout << "--- Classification  : Using input file: " << input->GetName() << std::endl;
+   std::cout << "--- Classification  : Using input file: " << input->GetName() << std::endl;
 
-    // Create a ROOT output file where TMVA will store ntuples, histograms, etc.
-    TString outfileName( "output.root" );
-    TFile* outputFile = TFile::Open(outfileName, "RECREATE");
+   // Create a ROOT output file where TMVA will store ntuples, histograms, etc.
+   TString outfileName( "output.root" );
+   TFile* outputFile = TFile::Open(outfileName, "RECREATE");
 
-    // Creating the factory object
-    TMVA::Factory *factory = new TMVA::Factory( "TMVAClassification", outputFile,
-                                                "!Correlations:!V:!Silent:Color:DrawProgressBar:AnalysisType=Classification:!ModelPersistence:Transformations=None" );
-    TMVA::DataLoader *dataloader = new TMVA::DataLoader("dataset_cnn_ecal");
+   // Creating the factory object
+   TMVA::Factory *factory = new TMVA::Factory( "TMVAClassification", outputFile,
+                                               "!Correlations:!V:!Silent:Color:DrawProgressBar:AnalysisType=Classification:!ModelPersistence:Transformations=None" );
+   TMVA::DataLoader *dataloader = new TMVA::DataLoader("dataset_cnn_ecal");
 
-    TTree *signalTree = (TTree*)input->Get("sig");
-    TTree *background = (TTree*)input->Get("bgk");
+   TTree *signalTree = (TTree*)input->Get("sgn");
+   TTree *background = (TTree*)input->Get("bkg");
 
-    // add variables (time zero and time 1)
-    for (int j = 0; j < 1024; ++j) {
-        TString varName = TString::Format("EBenergyRed[%d]", j);
-        dataloader->AddVariable(varName, 'F');
-    }
+   // add variables (time zero and time 1)
+   for (size_t j = 0; j < nx * ny; ++j) {
+      TString varName = TString::Format("var%zu", j);
+      dataloader->AddVariable(varName, 'F');
+   }
 
     dataloader->AddSignalTree    ( signalTree, 1.0 );
     dataloader->AddBackgroundTree( background, 1.0 );
@@ -83,9 +86,9 @@ void CNN_ECAL_test(TString archName) {
     TString batchLayoutString("BatchLayout=32|1|1024");
 
     // Training strategies.
-    TString training0("LearningRate=1e-1,Momentum=0.0,Repetitions=1,"
+    TString training0("LearningRate=1e-5,Momentum=0.0,Repetitions=1,"
                       "ConvergenceSteps=10,BatchSize=32,TestRepetitions=1,"
-                      "MaxEpochs=20,"
+                      "MaxEpochs=20,Optimizer=ADAM,"
                       "WeightDecay=1e-4,Regularization=None");
 
     TString trainingStrategyString ("TrainingStrategy=");
