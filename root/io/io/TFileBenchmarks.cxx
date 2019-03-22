f3e28dcb2c8cd9ea28eb2366bc7918807247de67
Alfonso <alfonso.cama@hotmail.com>
2019-03-22 18:55:18 +0100

    Removed unused includes and renamed benchmarks

--- root/io/io/TFileBenchmarks.cxx
+++ root/io/io/TFileBenchmarks.cxx
@@ -1,72 +1,65 @@
-#include "TROOT.h"   // ROOT::EnableImplicitMT
-
-#include "TFile.h"	
-#include "TFrame.h"
-#include "TRandom3.h"
-#include "TBenchmark.h"
-
 #include "ROOT/RDataFrame.hxx"
 #include "ROOT/RSnapshotOptions.hxx"
+#include "TRandom3.h"
 
 #include "benchmark/benchmark.h"
 #include "rootbench/RBConfig.h"
 
 
-static void BM_TFile_Write_ZLIB(benchmark::State &state) {
-	ROOT::EnableImplicitMT();
-	ROOT::RDF::RSnapshotOptions options;
-	options.fCompressionAlgorithm = ROOT::ECompressionAlgorithm::kZLIB;
-	//RCompressionSetting::EAlgorithm::EValues::kZLIB
+static void BM_TFile_RDFSnapshot_ZLIB(benchmark::State &state) {
+   ROOT::EnableImplicitMT();
+   ROOT::RDF::RSnapshotOptions options;
+   options.fCompressionAlgorithm = ROOT::ECompressionAlgorithm::kZLIB;
 
-	// We create an empty data frame
-   	ROOT::RDataFrame tdf(100000);
-   	// We now fill it with random numbers
-   	gRandom->SetSeed(1);
-   	auto tdf_1 = tdf.Define("rnd", []() { return gRandom->Gaus(); });
+   // We create an empty data frame
+   ROOT::RDataFrame tdf(100000);
+   // We now fill it with random numbers
+   gRandom->SetSeed(1);
+   auto tdf_1 = tdf.Define("rnd", []() { return gRandom->Gaus(); });
 
-	for (auto _ : state) {
-	   	//And we write out the dataset on disk
-	   	tdf_1.Snapshot("randomNumbers", "bench_data.root", "", options);
-	}
+   for (auto _ : state) {
+      //And we write out the dataset on disk
+      tdf_1.Snapshot("randomNumbers", "bench_data.root", "", options);
+   }
 }
-BENCHMARK(BM_TFile_Write_ZLIB)->Unit(benchmark::kMicrosecond);
+BENCHMARK(BM_TFile_RDFSnapshot_ZLIB)->Unit(benchmark::kMicrosecond);
 
 
-static void BM_TFile_Write_LZ4(benchmark::State &state) {
-	ROOT::EnableImplicitMT();
-	ROOT::RDF::RSnapshotOptions options;
-	options.fCompressionAlgorithm = ROOT::ECompressionAlgorithm::kLZ4;
+static void BM_TFile_RDFSnapshot_LZ4(benchmark::State &state) {
+   ROOT::EnableImplicitMT();
+   ROOT::RDF::RSnapshotOptions options;
+   options.fCompressionAlgorithm = ROOT::ECompressionAlgorithm::kLZ4;
 
-	// We create an empty data frame
-   	ROOT::RDataFrame tdf(100000);
-   	// We now fill it with random numbers
-   	gRandom->SetSeed(1);
-   	auto tdf_1 = tdf.Define("rnd", []() { return gRandom->Gaus(); });
+   // We create an empty data frame
+   ROOT::RDataFrame tdf(100000);
+   // We now fill it with random numbers
+   gRandom->SetSeed(1);
+   auto tdf_1 = tdf.Define("rnd", []() { return gRandom->Gaus(); });
 
-	for (auto _ : state) {
-	   	//And we write out the dataset on disk
-	   	tdf_1.Snapshot("randomNumbers", "bench_data.root", "", options);
-	}
+   for (auto _ : state) {
+      //And we write out the dataset on disk
+      tdf_1.Snapshot("randomNumbers", "bench_data.root", "", options);
+   }
 }
-BENCHMARK(BM_TFile_Write_LZ4)->Unit(benchmark::kMicrosecond);
+BENCHMARK(BM_TFile_RDFSnapshot_LZ4)->Unit(benchmark::kMicrosecond);
 
 
-static void BM_TFile_Write_kLZMA (benchmark::State &state) {
-	ROOT::EnableImplicitMT();
-	ROOT::RDF::RSnapshotOptions options;
-	options.fCompressionAlgorithm = ROOT::ECompressionAlgorithm::kLZMA;
+static void BM_TFile_RDFSnapshot_LZMA (benchmark::State &state) {
+   ROOT::EnableImplicitMT();
+   ROOT::RDF::RSnapshotOptions options;
+   options.fCompressionAlgorithm = ROOT::ECompressionAlgorithm::kLZMA;
 
-	// We create an empty data frame
-   	ROOT::RDataFrame tdf(100000);
-   	// We now fill it with random numbers
-   	gRandom->SetSeed(1);
-   	auto tdf_1 = tdf.Define("rnd", []() { return gRandom->Gaus(); });
+   // We create an empty data frame
+   ROOT::RDataFrame tdf(100000);
+   // We now fill it with random numbers
+   gRandom->SetSeed(1);
+   auto tdf_1 = tdf.Define("rnd", []() { return gRandom->Gaus(); });
 
-	for (auto _ : state) {
-	   	//And we write out the dataset on disk
-	   	tdf_1.Snapshot("randomNumbers", "bench_data.root", "", options);
-	}
+   for (auto _ : state) {
+      //And we write out the dataset on disk
+      tdf_1.Snapshot("randomNumbers", "bench_data.root", "", options);
+   }
 }
-BENCHMARK(BM_TFile_Write_kLZMA)->Unit(benchmark::kMicrosecond);
+BENCHMARK(BM_TFile_RDFSnapshot_LZMA)->Unit(benchmark::kMicrosecond);
 
 BENCHMARK_MAIN();
