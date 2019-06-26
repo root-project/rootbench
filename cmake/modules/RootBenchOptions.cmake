9e4ae25777d7b57b8a76dce01f3a626004c1b41c
Oksana Shadura <oksana.shadura@cern.ch>
2019-06-26 12:26:13 +0200

    Adding possibility to plug flamegraphes in rootbench

--- cmake/modules/RootBenchOptions.cmake
+++ cmake/modules/RootBenchOptions.cmake
@@ -3,4 +3,5 @@
 #
 # # TBD: to introduce special function for options (similar to root.git)
 #----------------------------------------------------------------------------
-set(coverage OFF)
+option(coverage "ROOTBench coverage" OFF)
+option(flamegraph "FlameGraph generation option" OFF)
