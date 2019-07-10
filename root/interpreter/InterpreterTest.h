cb4c22d19ba36b5c2f5a6540b0b6568b4b3c059a
HLilit <lilharutyunyan5@gmail.com>
2019-07-10 15:15:19 +0200

    Add memory measurements to interpreted PyROOT
    Improve memory measurements for ROOT interpreter benchmark

--- root/interpreter/InterpreterTest.h
+++ root/interpreter/InterpreterTest.h
@@ -11,39 +11,27 @@
 
 #include <chrono>
 
-#include "rootbench/RBConfig.h"
+//#include "rootbench/RBConfig.h"
+#include "rootbench/MemoryMeasurement.h"
 
-static int runTutorial(const std::string& dir, const std::string& filename, const std::string& perffile) {
-  std::string rootsys = RB::GetRootSys();
+static int runTutorial(const std::string& dir, const std::string& filename, std::string& perftutorial) {
   std::string rootInvocation;
-  std::string thisroot = rootsys + "/bin/thisroot.sh";
-  if (!filename.empty()) {
-    std::string fullpath = rootsys + "/" + dir + "/" + filename;
-    // FIXME: no source in /usr/dash
-    // We are writing /usr/bin/time -v output in file to get maximum resident memory for the benchmark
-    rootInvocation = "source \"" + thisroot + "\" && /usr/bin/time -v -o \"" + perffile + "\" root.exe -l -q -b -n -x \"" + fullpath + "\" -e return ";
-  } else {
-    rootInvocation = "source \"" + thisroot + "\" && /usr/bin/time -v -o \"" + perffile + "\" root.exe -l -q -b ";
-  }
-
+  std::string rootexe = "\" root.exe -l -q -b -n -x \"";
+  std::string endInvoc = "\" -e return ";
+  perftutorial = getMemoryStats(dir,filename,rootInvocation, rootexe, endInvoc);
     return std::system(rootInvocation.c_str());
 }
 
 static void TestTutorial(benchmark::State &state, const char *dir, const char *tutorial) {
   int peakSize = 0;
-  std::string perftutorial ("perfile.txt");
   for(auto _ : state){
       auto start = std::chrono::high_resolution_clock::now();
-      runTutorial(dir, tutorial,perftutorial);
-      auto end   = std::chrono::high_resolution_clock::now();
-      auto elapsed_seconds =
-      std::chrono::duration_cast<std::chrono::duration<double>>(
-        end - start);
+      std::string perftutorial="";
+      runTutorial(dir, tutorial, perftutorial);
+      auto end = std::chrono::high_resolution_clock::now();
+      auto elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
       state.SetIterationTime(elapsed_seconds.count());
-      std::string memorytutorial = "cat \"" + perftutorial + "\"| grep 'Maximum resident set size' | awk '{print $6}' > tmp_mem_file";
-      int res = std::system(memorytutorial.c_str());
-      (void) res;
-      std::ifstream("tmp_mem_file") >> peakSize;
+      peakSize = getMemoryMeasurement(perftutorial);
    }
    state.counters.insert({{"RSS", peakSize}});
 }
--- root/pyroot/PyROOTTest.h
+++ root/pyroot/PyROOTTest.h
@@ -11,25 +11,32 @@
 
 #include <chrono>
 
-#include "rootbench/RBConfig.h"
+//#include "rootbench/RBConfig.h"
+#include "rootbench/MemoryMeasurement.h"
 
-static int runTutorial(const std::string& dir, const std::string& filename) {
-  std::string rootsys = RB::GetRootSys();
-  std::string fullpath = rootsys + "/" + dir + "/" + filename;
-  std::string rootInvocation = "root -l -b -q -e 'TPython::Exec(\" exec( open(\"" + fullpath + "\").read())\")'";
+static int runTutorial(const std::string& dir, const std::string& filename, std::string& perftutorial) {
+  std::string rootInvocation;
+  std::string rootexe = "\" root -l -b -q -e 'TPython::Exec(\" exec( open('";
+  std::string endInvoc = "').read())\")'";
+  perftutorial = getMemoryStats(dir,filename,rootInvocation, rootexe, endInvoc);
     return std::system(rootInvocation.c_str());
+
+  // std::string rootsys = RB::GetRootSys();
+  // std::string fullpath = rootsys + "/" + dir + "/" + filename;
+  // std::string rootInvocation = "root -l -b -q -e 'TPython::Exec(\" exec( open(\"" + fullpath + "\").read())\")'";
+  //   return std::system(rootInvocation.c_str());
 }
 
 static void TestTutorial(benchmark::State &state, const char *dir, const char *tutorial) {
   int peakSize = 0;
   for(auto _ : state){
       auto start = std::chrono::high_resolution_clock::now();
-      runTutorial(dir, tutorial);
-      auto end   = std::chrono::high_resolution_clock::now();
-      auto elapsed_seconds =
-      std::chrono::duration_cast<std::chrono::duration<double>>(
-        end - start);
+      std::string perftutorial="";
+      runTutorial(dir, tutorial, perftutorial);
+      auto end = std::chrono::high_resolution_clock::now();
+      auto elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
       state.SetIterationTime(elapsed_seconds.count());
+      peakSize = getMemoryMeasurement(perftutorial);
    }
    state.counters.insert({{"RSS", peakSize}});
 }
