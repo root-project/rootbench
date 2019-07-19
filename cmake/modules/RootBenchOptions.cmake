03d44d98636c4a03c5856a93c7c4a4cab72da01c
HLilit <lilharutyunyan5@gmail.com>
2019-07-19 15:53:47 +0200

    Separate flamegraphs into memory and CPU generation options

--- cmake/modules/RootBenchOptions.cmake
+++ cmake/modules/RootBenchOptions.cmake
@@ -4,4 +4,5 @@
 # # TBD: to introduce special function for options (similar to root.git)
 #----------------------------------------------------------------------------
 option(coverage "ROOTBench coverage" OFF)
-option(flamegraph "FlameGraph generation option" OFF)
+option(flamegraphCPU "CPU FlameGraph generation option" OFF)
+option(flamegraphMem "Memory FlameGraph generation option" OFF)
