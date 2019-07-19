17cc65311b7ac02d78708431b628eb80ad36e443
HLilit <lilharutyunyan5@gmail.com>
2019-07-19 15:59:57 +0200

    Add patch truncated names for templates for flamegraphs

--- cmake/modules/FlameGraph.cmake
+++ cmake/modules/FlameGraph.cmake
@@ -3,9 +3,11 @@ include(ExternalProject)
 ExternalProject_Add(FlameGraph
 	GIT_REPOSITORY "https://github.com/brendangregg/FlameGraph.git"
 	UPDATE_COMMAND ""
-	PATCH_COMMAND ""
+	PATCH_COMMAND patch < ${CMAKE_SOURCE_DIR}/rootbench-scripts/stackcollapse-perf.patch
 	CONFIGURE_COMMAND ""
 	BUILD_COMMAND ""
 	INSTALL_COMMAND ""
 	EXCLUDE_FROM_ALL 1
 )
+
+
