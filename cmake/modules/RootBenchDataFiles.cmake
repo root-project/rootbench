136c61e8ff97997f887f7d392ed630ba4f3ed0d7
Oksana Shadura <oksana.shadura@cern.ch>
2020-02-14 15:48:10 +0100

    Add data generation for RNTuples (format is changing pretty often)

--- cmake/modules/RootBenchDataFiles.cmake
+++ cmake/modules/RootBenchDataFiles.cmake
@@ -10,6 +10,7 @@ function(download_file url filename) # hash_type hash
   if(NOT EXISTS ${filename})
     message(STATUS "INFO: downloading ${url}")
     file(DOWNLOAD ${url} ${filename}
+        # Currently there are no timeout, hash and TLS checks (to be added later)
         #TIMEOUT ${timeout}  # seconds
         #EXPECTED_HASH ${hash_type}=${hash}
         #TLS_VERIFY ON
@@ -18,8 +19,9 @@ function(download_file url filename) # hash_type hash
 endfunction(download_file)
 
 foreach(n ${compression_algorithms})
+  # TBD: Change names (remove tilda)!
   set(file_to_download "${rootbenchsite}/h1dst~${n}.root")
-  set(file_to_save "${PROJECT_BINARY_DIR}/rootbench_datafiles/h1ds-${n}.root")
+  set(file_to_save "${PROJECT_BINARY_DIR}/rootbench-datafiles/h1dst-${n}.root")
   download_file(${file_to_download} ${file_to_save})
 endforeach()
 
--- cmake/modules/RootBenchOptions.cmake
+++ cmake/modules/RootBenchOptions.cmake
@@ -4,4 +4,4 @@
 # # TBD: to introduce special function for options (similar to root.git)
 #----------------------------------------------------------------------------
 option(coverage OFF)
-option(rootbench_datafiles OFF)
+option(rootbench-datafiles OFF)
