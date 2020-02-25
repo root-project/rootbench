08c16eae0a99be61a127a023e1eaf755697dc451
Oksana Shadura <oksana.shadura@cern.ch>
2020-02-25 16:43:05 +0100

    Cleanup Linkdef file (thanks Philippe)

--- tools/datafile-generators/h1linkdef.h
+++ tools/datafile-generators/h1linkdef.h
@@ -5,11 +5,7 @@
 //  Created by Simon Leisibach on 25.09.19.
 //
 
-#ifdef __CINT__
-
-#pragma link off all globals;
-#pragma link off all classes;
-#pragma link off all functions;
+#ifdef __ROOTCLING__
 
 #pragma link C++ struct H1Event+;
 
