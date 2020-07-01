dcfeb5d608837e437dbf71a8edd67d9abc44e9b4
Oksana Shadura <oksana.shadura@cern.ch>
2020-07-01 10:05:54 +0200

    Rename old directory

--- travis-scripts/lcov-info-part1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source $HOME/.bashrc
-find $HOME/build -name "*.gcda" -exec cp "{}" $HOME/coveralls/ \;
-touch $HOME/build/coverage.info_part1
-echo "LCOV info part 1 (due complex setup of CI slaves for ROOT builds)"
-export LCOV_PATH_ROOTSYS="\"geninfo_adjust_src_path=/mnt/build/workspace/lcg_ext_rootcov/BUILDTYPE/Release/COMPILER/native/LABEL/ubuntu16/build/projects/ROOT-HEAD/src/ROOT-HEAD-build/=> `echo $ROOTSYS/`\""
-$HOME/travis-fix/travis_wait_log 5 $LCOV --gcov-tool gcov -c -d $HOME/coveralls/ --no-markers --quiet  --base-directory `pwd` --ignore-errors graph \
---rc $LCOV_PATH_ROOTSYS \
---output-file $HOME/build/coverage.info_part1
