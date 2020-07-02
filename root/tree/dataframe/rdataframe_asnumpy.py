b5546770560ccb9c2cc8e1504f50e654be6b8fb7
Stefan Wunsch <stefan.wunsch@cern.ch>
2020-07-02 09:06:30 +0200

    Add df102 RDF tutorial as benchmark
    * Add the tutorial as benchmark
    * Move the simple RDF (test) benchmark

--- root/tree/dataframe/rdataframe_asnumpy.py
+++ root/tree/dataframe/rdataframe_asnumpy.py
@@ -4,11 +4,11 @@ import pytest
 import ROOT
 
 
-def payload_test_asnumpy_simple():
+def payload_asnumpy_simple():
     df = ROOT.RDataFrame(10)
     x = df.Define('x', 'rdfentry_').AsNumpy(['x'])
     return np.sum(x['x'])
 
-def test_asnumpy_simple(benchmark):
-    r = benchmark(payload_test_asnumpy_simple)
+def test_rdataframe_asnumpy_simple(benchmark):
+    r = benchmark(payload_asnumpy_simple)
     assert(r == 45)
