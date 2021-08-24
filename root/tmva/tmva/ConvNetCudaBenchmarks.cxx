75bf66e165592d2fb8820daa263ef9296d94fd1c
afanto <85966358+afanto@users.noreply.github.com>
2021-08-24 11:45:55 -0700

    Update ConvNetCudaBenchmarks.cxx

--- root/tmva/tmva/ConvNetCudaBenchmarks.cxx
+++ root/tmva/tmva/ConvNetCudaBenchmarks.cxx
@@ -12,4 +12,15 @@ static void BM_ConvolutionalNetwork_CUDA(benchmark::State &state)
 }
 BENCHMARK(BM_ConvolutionalNetwork_CUDA);
 
+static void BM_ConvolutionalNetwork_Keras_CUDA(benchmark::State &state)
+{
+    TString architecture("GPU");
+
+    // Benchmarking
+    for (auto _ : state) {
+        CNN_Keras_benchmark(architecture);
+    }
+}
+BENCHMARK(BM_ConvolutionalNetwork_Keras_CUDA);
+
 BENCHMARK_MAIN();
