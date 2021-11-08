a5ee74f9df0fffc8e20e06e30eca666b1def344b
moneta <lorenzo.moneta@cern.ch>
2021-11-08 15:48:10 +0100

    Add new SOFIE benchmark tests based on the Session. Disable automatic generation of test code, using template benchmark is trivial to add a new test

--- root/tmva/sofie/SOFIEInference.cxx
+++ root/tmva/sofie/SOFIEInference.cxx
@@ -11,68 +11,40 @@
 #include <functional>
 #include <random>
 
-#include "input_models/compiled/Linear_16.hxx"
-#include "input_models/compiled/Linear_32.hxx"
-#include "input_models/compiled/Linear_64.hxx"
+#include "Conv_d100_L14_B1.hxx"
+#include "Conv_d100_L1_B1.hxx"
+//#include "input_models/compiled/Linear_16.hxx"
+///#include "input_models/compiled/Linear_32.hxx"
+//#include "input_models/compiled/Linear_64.hxx"
 //#include "input_models/compiled/Linear_event_blas.hxx" // old file 
-#include "input_models/compiled/Linear_event.hxx"   // generated from Linear_event.onnx
-#include "input_models/compiled/Conv_d100_L1_B1_opt.hxx"
+//#include "input_models/compiled/Linear_event.hxx"   // generated from Linear_event.onnx
+//#include "input_models/compiled/Conv_d100_L1_B1_opt.hxx"
 //#include "input_models/compiled/Conv_d100_L14_B1.hxx"
 
 using namespace std;
 bool verbose = false; 
-static void BM_SOFIE_Inference(benchmark::State &state, std::string model_name)
+template <class S>
+void BM_SOFIE_Inference(benchmark::State &state)
 {
     vector<float> input;
-    vector<float> (*infer_func)(float*);
-
-    if (model_name == "Linear_16") {
-        input.resize(1600);
-        infer_func = TMVA_SOFIE_Linear_16::infer;
-    } else if (model_name == "Linear_32") {
-        input.resize(3200);
-        infer_func = TMVA_SOFIE_Linear_32::infer;
-    } else if (model_name == "Linear_64") {
-        input.resize(6400);
-        infer_func = TMVA_SOFIE_Linear_64::infer;
-    } else if (model_name == "Linear_event") {
-        input.resize(100);
-        infer_func = TMVA_SOFIE_Linear_event::infer;
-    } else if (model_name == "Conv_d100_L1_B1") {
-        input.resize(100*100);
-        infer_func = TMVA_SOFIE_Conv_d100_L1_B1::infer;
-        TMVA_SOFIE_Conv_d100_L1_B1::init();
-    } /*else if (model_name == "Conv_d100_L1_B14") {
-       input.resize(100 * 100);
-       infer_func = TMVA_SOFIE_Conv_d100_L1_B14::infer;
-   }*/
+    input.resize(10000);
 
     static std::uniform_real_distribution<float> distribution(-1, 1);
     static std::default_random_engine generator;
     std::generate(input.begin(), input.end(), []() { return distribution(generator); });
 
     float *input_ptr = input.data();
-    if (model_name == "Conv_d100_L1_B1_temp") {
-       std::vector<float> output(2 * 100 * 100);
-       for (auto _ : state) {
-          //std::cout << "run for output " << output.size() << std::endl;
-          //TMVA_SOFIE_Conv_d100_L1_B1::infer(input_ptr, &output[0]);
-          // infer_func(input_ptr, &output[0];
-       }
-    }
-    else {
-       std::vector<float> output; 
-       for (auto _ : state) {
-          output = infer_func(input_ptr);
-        }
+    S s("");
+
+    
+    for (auto _ : state) {
+           s.infer(input.data());
     }
     //if (verbose) std::cout << "output : " << output.size() << " : " << output.front() << " ......" << output.back() << std::endl;
 }
-BENCHMARK_CAPTURE(BM_SOFIE_Inference, Linear_event, "Linear_event");
-BENCHMARK_CAPTURE(BM_SOFIE_Inference, Linear_16, "Linear_16");
-BENCHMARK_CAPTURE(BM_SOFIE_Inference, Linear_32, "Linear_32");
-BENCHMARK_CAPTURE(BM_SOFIE_Inference, Linear_64, "Linear_64");
-BENCHMARK_CAPTURE(BM_SOFIE_Inference, Conv_100_L1, "Conv_d100_L1_B1");
-//BENCHMARK_CAPTURE(BM_SOFIE_Inference, Conv_100_L14, "Conv_d100_L14_B1");
+//typedef TMVA_SOFIE_Conv_d100_L1_B1::Session S1;
+//BENCHMARK(BM_SOFIE_Inference<S1>);//->Name( "Conv_d100_L1_B1");
+BENCHMARK_TEMPLATE(BM_SOFIE_Inference, TMVA_SOFIE_Conv_d100_L1_B1::Session)->Name( "Conv_d100_L1_B1");
+BENCHMARK_TEMPLATE(BM_SOFIE_Inference, TMVA_SOFIE_Conv_d100_L14_B1::Session)->Name( "Conv_d100_L14_B1");
 
 BENCHMARK_MAIN();
---
+++ root/tmva/sofie/input_models/compiled/Conv_d100_L1_B1.hxx
@@ -0,0 +1,129 @@
+//Code generated automatically by TMVA for Inference of Model file [Conv_d100_L1_B1.onnx] at [Mon Nov  8 14:01:59 2021] 
+#include<vector>
+#include "TMVA/SOFIE_common.hxx"
+#include <fstream>
+
+namespace TMVA_SOFIE_Conv_d100_L1_B1{
+namespace BLAS{
+	extern "C" void saxpy_(const int * n, const float * alpha, const float * x,
+	                         const int * incx, float * y, const int * incy);
+	extern "C" void sgemm_(const char * transa, const char * transb, const int * m, const int * n, const int * k,
+	                       const float * alpha, const float * A, const int * lda, const float * B, const int * ldb,
+	                       const float * beta, float * C, const int * ldc);
+}//BLAS
+struct Session {
+float tensor_conv0bias[2] = {};
+float tensor_conv0weight[50] = {};
+std::vector<float> fTensor_conv0biasbcast = std::vector<float>(20000);
+float * tensor_conv0biasbcast = fTensor_conv0biasbcast.data();
+std::vector<float> fTensor_4 = std::vector<float>(20000);
+float * tensor_4 = fTensor_4.data();
+std::vector<float> fTensor_3 = std::vector<float>(20000);
+float * tensor_3 = fTensor_3.data();
+
+std::vector<float> fVec_op_0_f = std::vector<float>(50);
+std::vector<float> fVec_op_0_xpad = std::vector<float>(10816);
+std::vector<float> fVec_op_0_xcol = std::vector<float>(250000);
+
+
+Session(std::string filename ="") {
+   if (filename.empty()) filename = "Conv_d100_L1_B1.dat";
+   std::ifstream f;
+   f.open(filename);
+   if (!f.is_open()){
+      throw std::runtime_error("tmva-sofie failed to open file for input weights");
+   }
+   std::string tensor_name;
+   int length;
+   f >> tensor_name >> length;
+   if (tensor_name != "tensor_conv0bias" ) {
+      std::cout << "Error in tensor name : expected tensor name is tensor_conv0bias read " << tensor_name << std::endl;
+      throw std::runtime_error("tmva-sofie failed to read the correct tensor name");
+    }
+   if (length != 2) {
+      std::cout << "Error in tensor size : expected tensor size is 2 read " << length << std::endl;
+      throw std::runtime_error("tmva-sofie failed to read the correct tensor size");
+    }
+    for (int i =0; i < length; ++i) 
+       f >> tensor_conv0bias[i];
+   f >> tensor_name >> length;
+   if (tensor_name != "tensor_conv0weight" ) {
+      std::cout << "Error in tensor name : expected tensor name is tensor_conv0weight read " << tensor_name << std::endl;
+      throw std::runtime_error("tmva-sofie failed to read the correct tensor name");
+    }
+   if (length != 50) {
+      std::cout << "Error in tensor size : expected tensor size is 50 read " << length << std::endl;
+      throw std::runtime_error("tmva-sofie failed to read the correct tensor size");
+    }
+    for (int i =0; i < length; ++i) 
+       f >> tensor_conv0weight[i];
+   f.close();
+   {
+      std::vector<size_t> oldShape = { 2 };
+      std::vector<size_t> newShape = { 2, 100, 100};
+      oldShape.resize(newShape.size(), 1.);
+      float * newData_ptr = TMVA::Experimental::SOFIE::UTILITY::Unidirectional_broadcast<float>(tensor_conv0bias, oldShape, newShape);
+      int length = TMVA::Experimental::SOFIE::ConvertShapeToLength(newShape);
+      for (int i = 0; i < 1 ; i++)
+         std::copy(newData_ptr, newData_ptr + length , tensor_conv0biasbcast + i * length);
+   }
+}
+
+std::vector<float> infer(float* tensor_input){
+
+//----  operator Conv op_0
+	float * op_0_f = fVec_op_0_f.data();
+	for (std::size_t k = 0; k < 2; k++) {
+		for (std::size_t d = 0; d < 1; d++) {
+			for (std::size_t h = 0; h < 5; h++) {
+				for (std::size_t w = 0; w < 5; w++) {
+					op_0_f[k * 25 + d * 25 + h * 5 + w * 1  ] = tensor_conv0weight[k * 25 + d * 25 + h * 5 + w ];
+				}
+			}
+		}
+	}
+	float * op_0_xpad = fVec_op_0_xpad.data();
+	for (size_t c = 0; c < 1; c++) {
+		for (size_t h = 0; h < 100; h++) {
+			size_t xpad_offset = c * 10816 + (h + 2) * 104 + 2;
+			size_t x_offset = c * 10000 + h * 100;
+			std::copy(tensor_input + x_offset, tensor_input + x_offset + 100, op_0_xpad + xpad_offset);
+		}
+	}
+	char op_0_transA = 'T';
+	char op_0_transB = 'N';
+	int op_0_m = 10000;
+	int op_0_n = 2;
+	int op_0_k = 25;
+	float op_0_alpha = 1.0;
+	float op_0_beta = 0.0;
+	float * op_0_xcol = fVec_op_0_xcol.data();
+	for (size_t n = 0; n < 1; n++) {
+		size_t op_0_index = 0;
+		for (size_t h = 0; h < 100; h += 1) {
+			for (size_t w = 0; w < 100;w += 1) {
+				for (size_t c = 0; c < 1; c++) {
+					for (size_t x = 0; x < 5; x++) {
+					size_t offset =  c * 10816 + (h + x) * 104 + w;
+					std::copy(op_0_xpad + offset, op_0_xpad + offset + 5, op_0_xcol + op_0_index);
+					op_0_index += 5;
+					}
+				}
+			}
+		}
+		BLAS::sgemm_(&op_0_transA, &op_0_transB, &op_0_m, &op_0_n, &op_0_k, &op_0_alpha, op_0_xcol, &op_0_k,
+			op_0_f, &op_0_k, &op_0_beta, tensor_3, &op_0_m);
+	}
+	int op_0_size = 20000;
+	float op_0_gamma = 1.0;
+	int op_0_incx = 1;
+	int op_0_incy = 1;
+	BLAS::saxpy_(&op_0_size, &op_0_gamma, tensor_conv0biasbcast, &op_0_incx, tensor_3, &op_0_incy);
+	for (int id = 0; id < 20000 ; id++){
+		tensor_4[id] = ((tensor_3[id] > 0 )? tensor_3[id] : 0);
+	}
+	std::vector<float> ret (tensor_4, tensor_4 + 20000);
+	return ret;
+}
+};
+} //TMVA_SOFIE_Conv_d100_L1_B1
