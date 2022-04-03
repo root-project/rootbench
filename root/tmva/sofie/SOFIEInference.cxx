2797de5c25de0bde47aaba96bd2b093c22052901
moneta <lorenzo.moneta@cern.ch>
2022-04-03 11:40:19 +0200

    Add benchmark on CMS onnx file (DDB_B1.onnx) used with batch size=1
    Modifiy noth ONNXRuntimeinference and SOFIEINference to support models with multiple inputs

--- root/tmva/sofie/SOFIEInference.cxx
+++ root/tmva/sofie/SOFIEInference.cxx
@@ -25,16 +25,17 @@
 #include "GRU_d10_L20_h8_B1.hxx"
 #include "LSTM_d10_L20_h8_B1.hxx"
 #include "higgs_model_dense.hxx"
+#include "DDB_B1.hxx"   // CMS onnx model
 
 #include "resnet18v1.hxx"
 #include "TMath.h"
 
 
 using namespace std;
-bool verbose = false; 
+bool verbose = false;
 template <class S>
 void BM_SOFIE_Inference(benchmark::State &state)
-{ 
+{
    size_t inputSize = state.range(0);  // input size (without batch size)
    size_t bsize = (state.range(1) > 0) ? state.range(1) : 0;
    size_t nevts = 64;
@@ -74,8 +75,56 @@ void BM_SOFIE_Inference(benchmark::State &state)
    //  }
     //if (verbose) std::cout << "output : " << output.size() << " : " << output.front() << " ......" << output.back() << std::endl;
 }
-//typedef TMVA_SOFIE_Conv_d100_L1_B1::Session S1;
-//BENCHMARK(BM_SOFIE_Inference<S1>);//->Name( "Conv_d100_L1_B1");
+
+// inference for model with 3 inputs
+template <class S>
+void BM_SOFIE_Inference_3(benchmark::State &state)
+{
+   size_t bsize = state.range(0);  // batch size
+   size_t inputSize1 = state.range(1);  // input 1 size
+   size_t inputSize2 = state.range(2);  // input 2 size
+   size_t inputSize3 = state.range(3);
+
+   size_t nevts = 64;
+   size_t nrep = nevts / bsize;
+
+   size_t eventSize = inputSize1 + inputSize2+inputSize3;
+
+   vector<float> input1(inputSize1*nevts);
+   vector<float> input2(inputSize2*nevts);
+   vector<float> input3(inputSize3*nevts);
+
+   static std::uniform_real_distribution<float> distribution(-1, 1);
+   static std::default_random_engine generator;
+   std::generate(input1.begin(), input1.end(), []() { return distribution(generator); });
+   std::generate(input2.begin(), input2.end(), []() { return distribution(generator); });
+   std::generate(input3.begin(), input3.end(), []() { return distribution(generator); });
+
+   S s("");
+
+   std::cout << "initi done - do benchmark \n";
+
+   double totDuration = 0;
+   int ntimes = 0;
+   for (auto _ : state) {
+      auto t1 = std::chrono::high_resolution_clock::now();
+      for (int i = 0; i < nevts; i += bsize) {
+         float * p1 = input1.data()+ inputSize1*i;
+         float * p2 = input2.data()+ inputSize2*i;
+         float * p3 = input3.data()+ inputSize3*i;
+         auto y = s.infer(p1,p2,p3);
+      }
+      auto t2 = std::chrono::high_resolution_clock::now();
+      auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+      totDuration += duration / 1.E3;  // in milliseconds
+      ntimes++;
+   }
+
+   state.counters["time/evt(ms)"] = totDuration / double(ntimes * nevts);
+}
+
+// CMS benchmark (3 inputs)
+BENCHMARK_TEMPLATE(BM_SOFIE_Inference_3, TMVA_SOFIE_DDB_B1::Session)->Name("DDB_B1")->Args({1, 1*27, 60*8, 5*2})->Unit(benchmark::kMillisecond);
 
 //Gemm benchmarks
 BENCHMARK_TEMPLATE(BM_SOFIE_Inference, TMVA_SOFIE_Linear_16::Session)->Name("Linear_16")->Args({100, 16})->Unit(benchmark::kMillisecond);
