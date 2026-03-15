44dc4d44ead6faa436fe001796ce4d02b66ee268
Juan Miguel Carceller <jmcarcell@users.noreply.github.com>
2026-03-15 18:45:18 +0100

    Fix the location of some tutorials

--- root/interpreter/RunInterpreter.cxx
+++ root/interpreter/RunInterpreter.cxx
@@ -3,16 +3,16 @@
 #include "benchmark/benchmark.h"
 
 BENCHMARK_CAPTURE(TestTutorial, Test_startup, "", "")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_hsimple, "tutorials/", "hsimple.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_TMVAGAexample, "tutorials/", "tmva/TMVAGAexample.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_TMVAGAexample2, "tutorials/", "tmva/TMVAGAexample2.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_hvector, "tutorials/", "tree/hvector.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_hinvertMatrix, "tutorials/", "matrix/invertMatrix.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_fitLinear, "tutorials/", "fit/fitLinear.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_roofit103, "tutorials/", "roofit/rf103_interprfuncs.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_roofit105, "tutorials/", "roofit/rf105_funcbinding.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_roofit109, "tutorials/", "roofit/rf109_chi2residpull.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_roofit110, "tutorials/", "roofit/rf110_normintegration.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_roofit204, "tutorials/", "roofit/rf204_extrangefit.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_hsimple, "tutorials", "hsimple.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_TMVAGAexample, "tutorials", "machine_learning/TMVAGAexample.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_TMVAGAexample2, "tutorials", "machine_learning/TMVAGAexample2.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_hvector, "tutorials", "io/tree/tree121_hvector.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_hinvertMatrix, "tutorials", "math/matrix/invertMatrix.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_fitLinear, "tutorials", "math/fit/fitLinear.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_roofit103, "tutorials", "roofit/roofit/rf103_interprfuncs.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_roofit105, "tutorials", "roofit/roofit/rf105_funcbinding.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_roofit109, "tutorials", "roofit/roofit/rf109_chi2residpull.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_roofit110, "tutorials", "roofit/roofit/rf110_normintegration.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_roofit204, "tutorials", "roofit/roofit/rf204a_extendedLikelihood.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
 
 BENCHMARK_MAIN();
--- root/interpreter/RunInterpreterNoPCH.cxx
+++ root/interpreter/RunInterpreterNoPCH.cxx
@@ -2,23 +2,20 @@
 
 #include "benchmark/benchmark.h"
 
-BENCHMARK_CAPTURE(TestTutorial, Test_nopch_minuit2, "tutorials/", "fit/minuit2FitBench.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_nopch_minuit2Gaus, "tutorials/", "fit/minuit2GausFit.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_nopch_quadp, "tutorials/", "quadp/portfolio.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_nopch_foam, "tutorials/", "foam/foam_demo.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_nopch_fftw, "tutorials/", "fft/FFT.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_nopch_mlp_higgs, "tutorials/", "mlp/mlpHiggs.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_nopch_mlp_regression, "tutorials/", "mlp/mlpRegression.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_nopch_mlp_higgs, "tutorials/", "mlp/mlpHiggs.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_nopch_mlp_regression, "tutorials/", "mlp/mlpRegression.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_nopch_assembly, "tutorials/", "geom/assembly.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_nopch_na49, "tutorials/", "geom/na49.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_nopch_rootgeom, "tutorials/", "geom/rootgeom.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_nopch_parallel_world, "tutorials/", "geom/parallel_world.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_nopch_mp3player, "tutorials/", "geom/mp3player.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_nopch_geometry, "tutorials/", "geom/geometry.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_nopch_radionucl, "tutorials/", "geom/RadioNuclides.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_nopch_rootgeom, "tutorials/", "geom/rootgeom.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
-BENCHMARK_CAPTURE(TestTutorial, Test_nopch_xtruSamples, "tutorials/", "geom/xtruSamples.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_nopch_minuit2, "tutorials", "math/fit/minuit2FitBench.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_nopch_minuit2Gaus, "tutorials", "math/fit/minuit2GausFit.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_nopch_quadp, "tutorials", "math/quadp/portfolio.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_nopch_foam, "tutorials", "math/foam/foam_demo.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_nopch_fftw, "tutorials", "math/fft/FFT.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_nopch_mlp_higgs, "tutorials", "legacy/mlp/mlpHiggs.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_nopch_mlp_regression, "tutorials", "legacy/mlp/mlpRegression.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_nopch_assembly, "tutorials", "geom/assembly.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_nopch_na49, "tutorials", "legacy/g3d/na49.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_nopch_rootgeom, "tutorials", "visualisation/geom/rootgeom.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_nopch_parallel_world, "tutorials", "visualisation/geom/parallel_world.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_nopch_mp3player, "tutorials", "visualisation/geom/mp3player.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_nopch_geometry, "tutorials", "legacy/g3d/geometry.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_nopch_radionucl, "tutorials", "visualisation/geom/RadioNuclides.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
+BENCHMARK_CAPTURE(TestTutorial, Test_nopch_xtruSamples, "tutorials", "legacy/g3d/xtruSamples.C")->Unit(benchmark::kMicrosecond)->UseManualTime();
 
 BENCHMARK_MAIN();
