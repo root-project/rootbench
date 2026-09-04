e9c058d42f07f3cf58cdd0368c7af4c6679b6452
Jonas Rembser <jonas.rembser@cern.ch>
2026-09-04 14:06:02 +0000

    Remove PyKeras ConvNet benchmarks
    PyMVA was deprecated in ROOT, so it makes no sense to add benchmark
    coverage for it now. Furthermore, MethodPyKeras terminates the whole
    benchmark binary with an uncaught exception on machines where
    TensorFlow is not available, which would also take down the results of
    the native TMVA benchmarks in the same executable.
    
    This reverts the ConvNet benchmark files to their state on master, only
    keeping the missing-newline-at-end-of-file fixes.

--- root/tmva/tmva/ConvNetCpuBenchmarks.cxx
+++ root/tmva/tmva/ConvNetCpuBenchmarks.cxx
@@ -12,15 +12,4 @@ static void BM_ConvolutionalNetwork_CPU(benchmark::State &state)
 }
 BENCHMARK(BM_ConvolutionalNetwork_CPU);
 
-static void BM_ConvolutionalNetwork_Keras_CPU(benchmark::State &state)
-{
-    TString architecture("CPU");
-
-    // Benchmarking
-    for (auto _ : state) {
-        CNN_Keras_benchmark(architecture);
-    }
-}
-BENCHMARK(BM_ConvolutionalNetwork_Keras_CPU);
-
 BENCHMARK_MAIN();
--- root/tmva/tmva/ConvNetCudaBenchmarks.cxx
+++ root/tmva/tmva/ConvNetCudaBenchmarks.cxx
@@ -12,15 +12,4 @@ static void BM_ConvolutionalNetwork_CUDA(benchmark::State &state)
 }
 BENCHMARK(BM_ConvolutionalNetwork_CUDA);
 
-static void BM_ConvolutionalNetwork_Keras_CUDA(benchmark::State &state)
-{
-    TString architecture("GPU");
-
-    // Benchmarking
-    for (auto _ : state) {
-        CNN_Keras_benchmark(architecture);
-    }
-}
-BENCHMARK(BM_ConvolutionalNetwork_Keras_CUDA);
-
 BENCHMARK_MAIN();
